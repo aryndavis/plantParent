d3ca71704a831c4b71b0b28ac47533ea9070d207
Aryn Davis <arynlynn1@gmail.com>
2020-10-22 16:42:24 -0500

    installing python in CI didnt work

--- my_src/findAudio.py
+++ my_src/findAudio.py
@@ -4,9 +4,12 @@ import requests
 def find_audio(audioFile):
 
     data = {
-        'url': 'https://audd.tech/example1.mp3',
+        'url': 'audioFile',
         'return': 'apple_music,spotify',
         'api_token': 'test'
     }
     result = requests.post('https://api.audd.io/', data=data)
     print(result.text)
+
+
+find_audio('output.wav')
