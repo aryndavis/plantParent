0b920aa4c8d35dfab56dafa20adabbae268ecb26
Aryn Davis <arynlynn1@gmail.com>
2020-10-22 15:01:50 -0500

    record audio for 5 seconds!!!!

--- src/readAudio.py
+++ src/readAudio.py
@@ -1,40 +1,47 @@
 import pyaudio
 import wave
 
+
 def read_audio():
+    chunk = 1024  # Record in chunks of 1024 samples
+    sample_format = pyaudio.paInt16  # 16 bits per sample
+    channels = 1
+    fs = 44100  # Record at 44100 samples per second
+    seconds = 7
+    filename = "output.wav"
 
-    CHUNK = 1024
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 2
-    RATE = 44100
-    RECORD_SECONDS = 5
-    WAVE_OUTPUT_FILENAME = "output.wav"
+    p = pyaudio.PyAudio()  # Create an interface to PortAudio
 
-    p = pyaudio.PyAudio()
+    print('Recording')
 
-    stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                frames_per_buffer=CHUNK)
+    stream = p.open(format=sample_format,
+                    channels=channels,
+                    rate=fs,
+                    frames_per_buffer=chunk,
+                    input=True)
 
-    print("* recording")
+    frames = []  # Initialize array to store frames
 
-    frames = []
-
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-        data = stream.read(CHUNK)
+    # Store data in chunks for 3 seconds
+    for i in range(0, int(fs / chunk * seconds)):
+        data = stream.read(chunk)
         frames.append(data)
 
-    print("* done recording")
-
+    # Stop and close the stream
     stream.stop_stream()
     stream.close()
+    # Terminate the PortAudio interface
     p.terminate()
 
-    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(p.get_sample_size(FORMAT))
-    wf.setframerate(RATE)
+    print('Finished recording')
+
+    # Save the recorded data as a WAV file
+    wf = wave.open(filename, 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(sample_format))
+    wf.setframerate(fs)
     wf.writeframes(b''.join(frames))
     wf.close()
+
+
+read_audio()
