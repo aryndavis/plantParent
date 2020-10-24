538f10ee38f45adf126315df6a08b0253dcd8c9c
Aryn Davis <arynlynn1@gmail.com>
2020-10-23 23:43:06 -0500

    new tests + functions to make a call to get audio identification and print it in readable format

--- my_src/findAudio.py
+++ my_src/findAudio.py
@@ -4,12 +4,32 @@ import requests
 def find_audio(audioFile):
 
     data = {
-        'url': 'audioFile',
+        'url': 'https://audd.tech/example1.mp3',  # fake link, will need to fix
         'return': 'apple_music,spotify',
-        'api_token': 'test'
+        'api_token': 'test'  # api
     }
     result = requests.post('https://api.audd.io/', data=data)
-    print(result.text)
+    print(result.text, result.json()['status'])
+    if result.status_code == 200 and result.json()['status'] == 'success':
+        return result.json()
+    elif result.json()['status'] != 'success':
+        return 'error'
 
 
-find_audio('output.wav')
+def print_audio_nice(results):
+
+    sentence = ''
+    if results == 404 or results == 'error':
+        raise ValueError("this request didn't go through correctly")
+
+    artist = results['result']['artist']
+    title = results['result']['title']
+    album = results['result']['album']
+    print(f"The song is {title} by {artist} on the album {album}")
+    sentence = "The song is " + title
+    sentence += " by " + artist
+    sentence += " on the album " + album
+    return sentence
+
+# results = find_audio('output.wav')
+# print_audio_nice(results)
