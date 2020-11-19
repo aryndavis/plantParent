2ecf589d7a25b734a380b52fbefdc2d8f89e0167
Aryn Davis <arynlynn1@gmail.com>
2020-11-18 20:19:11 -0600

    working on applying lyric extraction from recorded files, need to integrate spleeter

--- my_src/findAudio.py
+++ my_src/findAudio.py
@@ -1,37 +1,18 @@
-import requests
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+import speech_recognition as sr
+import os
 
 
-def find_audio(audioFile):
+def find_lyrics(filename):
+    r = sr.Recognizer()
 
-    data = {
-        'url': 'https://audd.tech/example1.mp3',  # fake link, will need to fix
-        'return': 'apple_music,spotify',
-        'api_token': 'test'  # api
-    }
-    result = requests.post('https://api.audd.io/', data=data)
-    print(result.text, result.json()['status'])
-    if result.status_code == 200 and result.json()['status'] == 'success':
-        return result.json()
-    elif result.json()['status'] != 'success':
-        return 'error'
-    if result.status_code == 404:
-        return 404
+    with sr.AudioFile(filename) as source:
+        # listen for the data (load audio to memory)
+        audio_data = r.record(source)
+        # recognize (convert from speech to text)
+        text = r.recognize_google(audio_data, show_all=True)
+        print(text)
 
 
-def print_audio_nice(results):
-
-    sentence = ''
-    if results == 404 or results == 'error':
-        raise ValueError("this request didn't go through correctly")
-
-    artist = results['result']['artist']
-    title = results['result']['title']
-    album = results['result']['album']
-    print(f"The song is {title} by {artist} on the album {album}")
-    sentence = "The song is " + title
-    sentence += " by " + artist
-    sentence += " on the album " + album
-    return sentence
-
-# results = find_audio('output.wav')
-# print_audio_nice(results)
+find_lyrics('output.wav')
