6ac7979f0c8106fbe5c7b80d645e6682fba8bbb5
Aryn Davis <arynlynn1@gmail.com>
2020-12-08 17:00:05 -0600

    fixing flake8 errors

--- my_src/tests/tests.py
+++ my_src/tests/tests.py
@@ -5,6 +5,8 @@ import unittest
 # from unittest.mock import patch, MagicMock  # noqa: F401
 
 # class that all tests will go into
+
+
 class TestStuff(unittest.TestCase):
     # first test that i still have to make!
     def test_first(self):
