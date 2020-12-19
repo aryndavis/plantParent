57aa9f86e2ab8bbb8ca0170088b3dc3ad3b20222
Aryn Davis <arynlynn1@gmail.com>
2020-12-18 20:03:32 -0600

    fixed some slight flake8 issues

--- my_src/plant_illness.py
+++ my_src/plant_illness.py
@@ -10,7 +10,7 @@ def getIllConditions(plantName):
     model = AutoModelForQuestionAnswering.from_pretrained(
         "bert-large-uncased-whole-word-masking-finetuned-squad")
 
-    file = open("care_files/"+plantName+".txt", "r")
+    file = open("care_files/" + plantName + ".txt", "r")
     text = file.read()
 
     questions = [
@@ -23,17 +23,16 @@ def getIllConditions(plantName):
             question, text, add_special_tokens=True, return_tensors="pt")
         input_ids = inputs["input_ids"].tolist()[0]
 
-        text_tokens = tokenizer.convert_ids_to_tokens(input_ids)
         answer_start_scores, answer_end_scores = model(**inputs)
 
-        answer_start = torch.argmax(
-            answer_start_scores
-        )  # Get the most likely beginning of answer with the argmax of the score
+        # Get the most likely beginning of answer with the argmax of the score
+        answer_start = torch.argmax(answer_start_scores)
         # Get the most likely end of answer with the argmax of the score
         answer_end = torch.argmax(answer_end_scores) + 1
 
         answer = tokenizer.convert_tokens_to_string(
-            tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
+            tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end])
+            )
 
         print(f"Question: {question}")
         print(f"Answer: {answer}\n")
--- my_src/plant_soil.py
+++ my_src/plant_soil.py
@@ -3,15 +3,15 @@ import torch
 import os
 os.chdir('my_src/')
 
+
 def getSoilConditions(plantName):
     tokenizer = AutoTokenizer.from_pretrained(
         "bert-large-uncased-whole-word-masking-finetuned-squad")
     model = AutoModelForQuestionAnswering.from_pretrained(
         "bert-large-uncased-whole-word-masking-finetuned-squad")
 
-    file = open("care_files/"+plantName+".txt", "r")
+    file = open("care_files/" + plantName + ".txt", "r")
     text = file.read()
-
 
     questions = [
         "How should we treat the soil of this plant?",
@@ -23,17 +23,16 @@ def getSoilConditions(plantName):
             question, text, add_special_tokens=True, return_tensors="pt")
         input_ids = inputs["input_ids"].tolist()[0]
 
-        text_tokens = tokenizer.convert_ids_to_tokens(input_ids)
         answer_start_scores, answer_end_scores = model(**inputs)
 
-        answer_start = torch.argmax(
-            answer_start_scores
-        )  # Get the most likely beginning of answer with the argmax of the score
+        # Get the most likely beginning of answer with the argmax of the score
+        answer_start = torch.argmax(answer_start_scores)
         # Get the most likely end of answer with the argmax of the score
         answer_end = torch.argmax(answer_end_scores) + 1
 
         answer = tokenizer.convert_tokens_to_string(
-            tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
+            tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end])
+            )
 
         print(f"Question: {question}")
         print(f"Answer: {answer}\n")
