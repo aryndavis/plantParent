f8c6f680c7530e9f5f75a917bfd8ea7b73501b36
Aryn Davis <arynlynn1@gmail.com>
2020-12-20 01:11:22 -0600

    adding necessary exports for testing

---
+++ my_src/__init__.py
@@ -0,0 +1,4 @@
+from .plant_illness import getIllConditions  # noqa: F401
+from .plant_soil import getSoilConditions  # noqa: F401
+from .plant_sun import getSunConditions  # noqa: F401
+from .plant_water import getWaterConditions  # noqa: F401
